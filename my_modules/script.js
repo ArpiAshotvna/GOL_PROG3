499bc90e0be53666ba91f29ef18a01fbd94cbd6b
ArpiAshotvna <arpi.hayrapetyan.y@gmail.com>
2023-02-20 20:33:34 +0400

    n

--- my_modules/script.js
+++ my_modules/script.js
@@ -1,130 +1,142 @@
-function generateMatrix(matLength, gr, grEa, pr, bmb, ml) {
-    let matrix = [];
-    for (let i = 0; i < matLength; i++) {
-        matrix.push([])
-        for (let j = 0; j < matLength; j++) {
-            matrix[i].push(0)
-        }
-    }
-    for (let i = 0; i < gr; i++) {
-        let x = Math.floor(Math.random() * matLength);
-        let y = Math.floor(Math.random() * matLength);
-        if (matrix[y][x] == 0) {
-            matrix[x][y] = 1;
-        }
-    }
-    for (let i = 0; i < grEa; i++) {
-        let x = Math.floor(Math.random() * matLength);
-        let y = Math.floor(Math.random() * matLength);
-        if (matrix[y][x] == 0) {
-            matrix[x][y] = 2;
-        }
-    }
-    for (let i = 0; i < pr; i++) {
-        let x = Math.floor(Math.random() * matLength);
-        let y = Math.floor(Math.random() * matLength);
-        if (matrix[y][x] == 0) {
-            matrix[x][y] = 3;
-        }
-    }
-    for (let i = 0; i < bmb; i++) {
-        let x = Math.floor(Math.random() * matLength);
-        let y = Math.floor(Math.random() * matLength);
-        if (matrix[y][x] == 0) {
-            matrix[x][y] = 4;
-        }
-    }
 
-    for (let i = 0; i < ml; i++) {
-        let x = Math.floor(Math.random() * matLength);
-        let y = Math.floor(Math.random() * matLength);
-        if (matrix[y][x] == 0) {
-            matrix[x][y] = 5;
-        }
-    }
-    return matrix;
-}
-let matrix = generateMatrix(25, 45, 5, 5, 5, 5);
+var socket = io();
 
-var side = 30;
-let grassArr = [];
-let grassEaterArr = [];
-let PredatorArr = [];
-let bombArr = [];
-let MulArr = [];
-function setup() {
-    frameRate(5);
-    createCanvas(matrix[0].length * side, matrix.length * side);
-    background('#acacac');
-
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                let gr = new Grass(x, y);
-                grassArr.push(gr)
-            } else if (matrix[y][x] == 2) {
-                let grEat = new GrassEater(x, y);
-                grassEaterArr.push(grEat)
-            }
-            else if (matrix[y][x] == 3) {
-                let pr = new Predator(x, y);
-                PredatorArr.push(pr)
-            }
-            else if (matrix[y][x] == 4) {
-                let bm = new Bomb(x, y);
-                bombArr.push(bm)
-            }
-            else if (matrix[y][x] == 5) {
-                let ml = new Mul(x, y);
-                MulArr.push(ml)
-            }
-        }
-    }
-
+function handleMatrix(matrix){
+    console.log(matrix)
 }
 
-function draw() {
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
 
-            if (matrix[y][x] == 1) {
-                fill("green");
-            }
-            else if (matrix[y][x] == 0) {
-                fill("#acacac");
-            } else if (matrix[y][x] == 2) {
-                fill("yellow");
-            }
-            else if (matrix[y][x] == 3) {
-                fill("red");
-            }
-            else if (matrix[y][x] == 4) {
-                fill("black");
-            }
-            else if (matrix[y][x] == 5) {
-                fill("blue");
-            }
-
-            rect(x * side, y * side, side, side);
+socket.on("'send matrix", handleMatrix)
 
 
-        }
-    }
 
-    for (let i in grassArr) {
-        grassArr[i].mul()
-    }
+// function generateMatrix(matLength, gr, grEa, pr, bmb, ml) {
+//     let matrix = [];
+//     for (let i = 0; i < matLength; i++) {
+//         matrix.push([])
+//         for (let j = 0; j < matLength; j++) {
+//             matrix[i].push(0)
+//         }
+//     }
+//     for (let i = 0; i < gr; i++) {
+//         let x = Math.floor(Math.random() * matLength);
+//         let y = Math.floor(Math.random() * matLength);
+//         if (matrix[y][x] == 0) {
+//             matrix[x][y] = 1;
+//         }
+//     }
+//     for (let i = 0; i < grEa; i++) {
+//         let x = Math.floor(Math.random() * matLength);
+//         let y = Math.floor(Math.random() * matLength);
+//         if (matrix[y][x] == 0) {
+//             matrix[x][y] = 2;
+//         }
+//     }
+//     for (let i = 0; i < pr; i++) {
+//         let x = Math.floor(Math.random() * matLength);
+//         let y = Math.floor(Math.random() * matLength);
+//         if (matrix[y][x] == 0) {
+//             matrix[x][y] = 3;
+//         }
+//     }
+//     for (let i = 0; i < bmb; i++) {
+//         let x = Math.floor(Math.random() * matLength);
+//         let y = Math.floor(Math.random() * matLength);
+//         if (matrix[y][x] == 0) {
+//             matrix[x][y] = 4;
+//         }
+//     }
 
-    for (let i in grassEaterArr) {
-        grassEaterArr[i].eat()
-    }
-    for (let i in PredatorArr) {
-        PredatorArr[i].eat()
-    }
-    for (let i in bombArr) {
-        bombArr[i].bomb()
-    }
-    for (let i in MulArr) {
-        MulArr[i].mul()
-    }
-}
+//     for (let i = 0; i < ml; i++) {
+//         let x = Math.floor(Math.random() * matLength);
+//         let y = Math.floor(Math.random() * matLength);
+//         if (matrix[y][x] == 0) {
+//             matrix[x][y] = 5;
+//         }
+//     }
+//     return matrix;
+// }
+// let matrix = generateMatrix(25, 45, 5, 5, 5, 5);
+
+// var side = 30;
+// let grassArr = [];
+// let grassEaterArr = [];
+// let PredatorArr = [];
+// let bombArr = [];
+// let MulArr = [];
+// function setup() {
+//     frameRate(5);
+//     createCanvas(matrix[0].length * side, matrix.length * side);
+//     background('#acacac');
+
+//     for (var y = 0; y < matrix.length; y++) {
+//         for (var x = 0; x < matrix[y].length; x++) {
+//             if (matrix[y][x] == 1) {
+//                 let gr = new Grass(x, y);
+//                 grassArr.push(gr)
+//             } else if (matrix[y][x] == 2) {
+//                 let grEat = new GrassEater(x, y);
+//                 grassEaterArr.push(grEat)
+//             }
+//             else if (matrix[y][x] == 3) {
+//                 let pr = new Predator(x, y);
+//                 PredatorArr.push(pr)
+//             }
+//             else if (matrix[y][x] == 4) {
+//                 let bm = new Bomb(x, y);
+//                 bombArr.push(bm)
+//             }
+//             else if (matrix[y][x] == 5) {
+//                 let ml = new Mul(x, y);
+//                 MulArr.push(ml)
+//             }
+//         }
+//     }
+
+// }
+
+// function draw() {
+//     for (var y = 0; y < matrix.length; y++) {
+//         for (var x = 0; x < matrix[y].length; x++) {
+
+//             if (matrix[y][x] == 1) {
+//                 fill("green");
+//             }
+//             else if (matrix[y][x] == 0) {
+//                 fill("#acacac");
+//             } else if (matrix[y][x] == 2) {
+//                 fill("yellow");
+//             }
+//             else if (matrix[y][x] == 3) {
+//                 fill("red");
+//             }
+//             else if (matrix[y][x] == 4) {
+//                 fill("black");
+//             }
+//             else if (matrix[y][x] == 5) {
+//                 fill("blue");
+//             }
+
+//             rect(x * side, y * side, side, side);
+
+
+//         }
+//     }
+
+//     for (let i in grassArr) {
+//         grassArr[i].mul()
+//     }
+
+//     for (let i in grassEaterArr) {
+//         grassEaterArr[i].eat()
+//     }
+//     for (let i in PredatorArr) {
+//         PredatorArr[i].eat()
+//     }
+//     for (let i in bombArr) {
+//         bombArr[i].bomb()
+//     }
+//     for (let i in MulArr) {
+//         MulArr[i].mul()
+//     }
+// }
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var express = require("express");
-
+matrix = [1, 1, 1]
+var express = require('express');
 var app = express();
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var messages = [];
 app.use(express.static("my_modules"));
-
-app.get("/", function(req, res){
-
-res.redirect("index.html");
-
+app.get('/', function (req, res) {
+    res.redirect('index.html');
 });
+server.listen(3000);
 
-app.listen(3000, function(){
 
-console.log("Example is running on port 3000");
-
-});
+io.on('connection', function (socket) {
+    socket.emit("send matrix", matrix);
+})
